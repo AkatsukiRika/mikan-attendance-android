fe8aad15c612355aba1106ae51da2a145200fec8
AkatsukiRika <m1345860061@gmail.com>
2020-07-02 17:22:48 +0800

    修复部分布局问题

--- app/src/main/java/com/example/mikanattendance/base/BaseConfigurations.java
+++ app/src/main/java/com/example/mikanattendance/base/BaseConfigurations.java
@@ -1,6 +1,6 @@
 package com.example.mikanattendance.base;
 
 public class BaseConfigurations {
-//    public static String baseUrl = "http://111.230.131.247:8212/";
-    public static String baseUrl = "http://192.168.0.100:8080/";
+    public static String baseUrl = "http://111.230.131.247:8212/";
+//    public static String baseUrl = "http://192.168.0.100:8080/";
 }
